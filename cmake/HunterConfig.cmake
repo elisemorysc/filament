140c590849c02bac1623202b51d18869a836a036
Eli Semory <elisemory@snap.com>
2020-10-28 18:14:50 -0400

    remove defaults packages

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,14 +1,6 @@
 hunter_config(civetweb VERSION 1.11-p0
               CMAKE_ARGS CIVETWEB_ENABLE_WEBSOCKETS=ON)
 
-hunter_config(smol-v VERSION 0.0.0-4b52c16-p0)
-
-hunter_config(benchmark VERSION 1.5.0)
-
-hunter_config(draco VERSION 1.3.4-p0)
-
-hunter_config(GTest VERSION 1.10.0-p1)
-
 hunter_config(SPIRV-Headers VERSION 1.5.1)
 
 hunter_config(glslang 
@@ -25,14 +17,6 @@ hunter_config(spirv-cross VERSION 20200917)
 hunter_config(astc-encoder
               URL https://github.com/cpp-pm/astc-encoder/archive/1.3-a47b80f-p0.zip
               SHA1 0143896afc679c05171a20a46217bbc0b8a5896d)
-
-hunter_config(etc2comp VERSION 0.0.0-9cd0f9c-p0)
-
-hunter_config(Assimp VERSION 5.0.1-dbd99bf)
-
-hunter_config(tinyexr VERSION 1.0.0-297e645-p0)
-
-
 
                 
   
