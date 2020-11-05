7fa542095cf1394181e6705934c18875a1baa25f
Eli Semory <elisemory@snap.com>
2020-11-05 15:59:58 -0500

    update hunter version

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,14 +1,13 @@
 hunter_config(civetweb VERSION 1.11-p0
               CMAKE_ARGS CIVETWEB_ENABLE_WEBSOCKETS=ON)
+
 hunter_config(SPIRV-Headers VERSION 1.5.1.corrected)
+
 hunter_config(SPIRV-Tools VERSION 2020.1-p0)
+
 hunter_config(spirv-cross VERSION 20200917)
 
-hunter_config(glslang 
-              URL https://github.com/elisemorysc/glslang/archive/9eef54b2-p0.tar.gz
-              SHA1 f99ff5307ee891ed57aed6efd5097816bffe5c7f
+hunter_config(glslang VERSION 8.13.3743-9eef54b2-p0
               CMAKE_ARGS ENABLE_HLSL=OFF ENABLE_GLSLANG_BINARIES=OFF ENABLE_OPT=OFF BUILD_TESTING=OFF)
-
-hunter_config(astc-encoder
-              URL https://github.com/cpp-pm/astc-encoder/archive/1.3-a47b80f-p0.zip
-              SHA1 0143896afc679c05171a20a46217bbc0b8a5896d)
+              
+hunter_config(astc-encoder VERSION 1.3-a47b80f-p0)
